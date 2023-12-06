5d8220d2db979b87d359df933373ba68ea4ac2e6
CodingisCool <edifice1989@gmail.com>
2023-12-06 14:25:07 -0800

    update class names

--- models/model/transfomer.py
+++ models/model/transfomer.py
@@ -8,9 +8,10 @@ from models.model import Decoder
 from models.layers import EncoderLayer
 from models.layers import DecoderLayer
 
-from models.layers import multi_head_attention
-from models.layers import feed_forward_layer
-from models.layers import positional_encoding
+from models.layers import PositionalEncoding
+from models.layers import MultiHeadAttention
+from models.layers import FeedForwardLayer
+
 
 from models.func import Embeddings
 from models.func import Generator
@@ -20,18 +21,18 @@ def make_model(
 
     c = copy.deepcopy
 
-    pos = positional_encoding(d_model) 
+    pos = PositionalEncoding(d_model) 
 
-    attn = multi_head_attention(h, d_model)
+    attn = MultiHeadAttention(h, d_model)
 
-    ff = feed_forward_layer(d_model, d_ff)
+    ff = FeedForwardLayer(d_model, d_ff)
 
     model = EncodeDecode(
         Encoder(EncoderLayer(d_model, c(attn), c(ff), dropout), N),
         Decoder(DecoderLayer(d_model, c(attn), c(attn), c(ff), dropout), N),
         nn.Sequential(Embeddings(d_model, src_vocab), c(pos)),#src embedded
         nn.Sequential(Embeddings(d_model, tgt_vocab), c(pos)), #target embedded
-        generator(d_model, tgt_vocab),
+        Generator(d_model, tgt_vocab),
     )
     for p in model.parameters():
         if p.dim() > 1:
