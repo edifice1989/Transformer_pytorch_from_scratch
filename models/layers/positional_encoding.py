35d622137c83287cec0d7fc11bdab9b74db472f4
CodingisCool <edifice1989@gmail.com>
2023-12-05 19:10:19 -0800

    positional_encoding

---
+++ models/layers/positional_encoding.py
@@ -0,0 +1,28 @@
+import torch
+import torch.nn as nn
+
+
+class PositionalEncoding(nn.module):
+    
+    def __init__(self,d_model,max_len):
+
+        super(PositionalEncoding).__init__()
+
+        self.encoding = torch.zeros(max_len,d_model) # figure out the order of those two, 
+        # how to use them in the below section
+
+        pos = torch.range(0,max_len).float()
+
+        pos = pos.unsqueeze(dim=1)
+
+        _i2 = torch.arrange(0,d_model,step=2).float()
+
+        self.encoding[:,0::2] = torch.sin(pos/10000**(_i2/d_model))
+
+        self.encoding[:,1::2] = torch.cos(pos/10000**(_i2/d_model))
+
+    def forward(self,x):
+        
+        batch_size, seq_len = x.size() 
+
+        return self.encoding[:seq_len, :]
